2a65f482b273eb4741e249489f74a206be6cbfb4
fujas <50135168+fujas@users.noreply.github.com>
2020-11-01 21:57:25 +0900

    世継ぎ取得関数など。

--- test/keizu_main.js
+++ test/keizu_main.js
@@ -1,5 +1,7 @@
 // 系図作成プログラム
 
+// ********** 数値制御関数 **********
+
 // 新規人物のIDを割り振る関数
 let g_ID = 0;
 function resetNewID() {
@@ -20,9 +22,10 @@ function defineNumChild() {
   return 2;
 }
 
+// ********** 人物クラス **********
 
-// 人物クラス
 let Person = (function () {
+
   // コンストラクタ
   let Person = function (parent, id, generation, male) {
     if (!(this instanceof Person)) {
@@ -34,12 +37,15 @@ let Person = (function () {
     this.generation = generation;   // 世代。起点以前なら負の数になる。
     this.male = male;               // 性別(trueで男)
     this.child = [];                // 子供へのポインタ配列
+    // メンバ変数（内部フラグ）
+    this.childCreated = false;      // 子供を全部作ったらtrue
+    this.noChild = false;           // 子孫が途絶えることが確定したらtrue
   }
   let p = Person.prototype;
 
   // 子供生成関数
   p.createChildren = function () {
-    if (this.male) {
+    if (this.male && this.childCreated == false) {
       let currChild = this.child.length;  // 既に存在する子供の数
       let numChild = defineNumChild();    // 生まれるべき全部の子供の数
       let gene = this.generation + 1;     // １世代あとに設定
@@ -48,6 +54,7 @@ let Person = (function () {
         // 子供を生成。性別は確率で決まる
         this.child[i] = new Person(this, getNewID(), gene, defineMale());
       }
+      this.childCreated = true;
     }
   }
 
@@ -56,12 +63,48 @@ let Person = (function () {
     let gene = this.generation - 1;
     let parent= new Person(null, getNewID(), gene, true); // １世代前の男として生成
     parent.child[0] = this;     // 必ず自分を子供に設定
+    parent.createChildren();    // 自分以外の子供を生成
     return parent;
+  }
+
+  // 世継ぎ取得関数
+  p.getPrince = function(){
+    // 子供を作っていなければ生成
+    this.createChildren();
+    // 子供に男がいればそれを返す
+    for (let child of this.child){
+      if (child.male){
+        return child;
+      }
+    }
+    // 男がいなければnull
+    return null;
   }
 
   return Person;
 })();
 
+// ********** １世代先の世継ぎを生成する関数群 **********
+
+function createPrince(person){
+  person.createChildren();
+  let prince = person.getPrince();
+  if (prince == null){
+
+
+
+    // backTrackPrince(person, person.generation + 1);
+
+
+
+  }
+  return prince;
+}
+
+
+
+
+// ********** 表示関数 **********
 
 // 表示
 function display() {
@@ -87,7 +130,8 @@ function display() {
   var network = new vis.Network(container, data, options);
 }
 
-// メイン
+// ********** メイン **********
+
 let origin = new Person(null, getNewID(), 1, true);
 origin.createChildren();
 origin.createParent();
