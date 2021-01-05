fbc7bac1a5b64285658f72ec46467d30f0903f05
fujas <furutorijasmine@gmail.com>
2021-01-05 21:20:35 +0900

    乱数をxorshift128+に変更。

--- app/keizu_tree.js
+++ app/keizu_tree.js
@@ -5,6 +5,122 @@
 // 系図の起点は「未来の架空の天皇」であり、その天皇まで同じ条件で継承されたものと仮定します。
 //
 
+// ********** シード付き乱数 **********
+
+// xorshift128+ 
+// Copyright (c) 2014 Andreas Madsen & Emil Bay
+/**
+ * Create a pseudorandom number generator, with a seed.
+ * @param {array} seed "128-bit" integer, composed of 4x32-bit
+ * integers in big endian order.
+ */
+function XorShift(seed) {
+  // Note the extension, this === module.exports is required because
+  // the `constructor` function will be used to generate new instances.
+  // In that case `this` will point to the default RNG, and `this` will
+  // be an instance of XorShift.
+  if (!(this instanceof XorShift) /*|| this === module.exports*/) {
+    return new XorShift(seed);
+  }
+
+  if (!Array.isArray(seed) || seed.length !== 4) {
+    throw new TypeError('seed must be an array with 4 numbers');
+  }
+
+  // uint64_t s = [seed ...]
+  this._state0U = seed[0] | 0;
+  this._state0L = seed[1] | 0;
+  this._state1U = seed[2] | 0;
+  this._state1L = seed[3] | 0;
+}
+
+/**
+ * Returns a 64bit random number as a 2x32bit array
+ * @return {array}
+ */
+XorShift.prototype.randomint = function() {
+  // uint64_t s1 = s[0]
+  var s1U = this._state0U, s1L = this._state0L;
+  // uint64_t s0 = s[1]
+  var s0U = this._state1U, s0L = this._state1L;
+
+  // result = s0 + s1
+  var sumL = (s0L >>> 0) + (s1L >>> 0);
+  resU = (s0U + s1U + (sumL / 2 >>> 31)) >>> 0;
+  resL = sumL >>> 0;
+
+  // s[0] = s0
+  this._state0U = s0U;
+  this._state0L = s0L;
+
+  // - t1 = [0, 0]
+  var t1U = 0, t1L = 0;
+  // - t2 = [0, 0]
+  var t2U = 0, t2L = 0;
+
+  // s1 ^= s1 << 23;
+  // :: t1 = s1 << 23
+  var a1 = 23;
+  var m1 = 0xFFFFFFFF << (32 - a1);
+  t1U = (s1U << a1) | ((s1L & m1) >>> (32 - a1));
+  t1L = s1L << a1;
+  // :: s1 = s1 ^ t1
+  s1U = s1U ^ t1U;
+  s1L = s1L ^ t1L;
+
+  // t1 = ( s1 ^ s0 ^ ( s1 >> 17 ) ^ ( s0 >> 26 ) )
+  // :: t1 = s1 ^ s0
+  t1U = s1U ^ s0U;
+  t1L = s1L ^ s0L;
+  // :: t2 = s1 >> 18
+  var a2 = 18;
+  var m2 = 0xFFFFFFFF >>> (32 - a2);
+  t2U = s1U >>> a2;
+  t2L = (s1L >>> a2) | ((s1U & m2) << (32 - a2));
+  // :: t1 = t1 ^ t2
+  t1U = t1U ^ t2U;
+  t1L = t1L ^ t2L;
+  // :: t2 = s0 >> 5
+  var a3 = 5;
+  var m3 = 0xFFFFFFFF >>> (32 - a3);
+  t2U = s0U >>> a3;
+  t2L = (s0L >>> a3) | ((s0U & m3) << (32 - a3));
+  // :: t1 = t1 ^ t2
+  t1U = t1U ^ t2U;
+  t1L = t1L ^ t2L;
+
+  // s[1] = t1
+  this._state1U = t1U;
+  this._state1L = t1L;
+
+  // return result
+  return [resU, resL];
+};
+
+/**
+ * Returns a random number normalized [0, 1), just like Math.random()
+ * @return {number}
+ */
+XorShift.prototype.random = function() {
+  var t2 = this.randomint();
+  // Math.pow(2, -32) = 2.3283064365386963e-10
+  // Math.pow(2, -52) = 2.220446049250313e-16
+  var rnd = t2[0] * 2.3283064365386963e-10 + (t2[1] >>> 12) * 2.220446049250313e-16;
+  return rnd;
+};
+// xorshift128+
+
+let g_myRnd;
+
+function resetRnd(seed){
+  // パターンIDを乱数シードにする
+  g_myRnd = new XorShift([seed + 12345, seed * 2 + 101, seed * 3 + 12345, seed + 5678]);
+  // 初期の乱数は偏りが大きいので何回か乱数生成
+  for (let i = 0; i < 5; i++){
+    g_myRnd.randomint();
+  }
+}
+
 // ********** 構造体 ***********
 // 生成情報パラメーター
 function TreeStat(){
@@ -28,40 +144,6 @@ function StatisticsParams(){
 let g_Statistics;
 let g_Params;
 
-
-// ********** シード付き乱数 **********
-// "JavaScriptで再現性のある乱数を生成する + 指定した範囲の乱数を生成する" を参考にさせていただきました。
-let Random = (function () {
-  // コンストラクタ
-  let Random = function (seed) {
-    if (!(this instanceof Random)) {
-      return new Random();
-    }
-    this.x = 123456789;
-    this.y = 362436069;
-    this.z = 521288629;
-    this.w = seed;
-  }
-  let r = Random.prototype;
-
-  // XorShiftで0-1の乱数発生
-  r.get = function () {
-    let t = this.x ^ (this.x << 11);
-    this.x = this.y; this.y = this.z; this.z = this.w;
-    this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8));
-    const limit = 10000;
-    let random = (Math.abs(this.w) % limit) / limit;
-    return random;
-  }
-
-  return Random;
-})();
-let g_myRnd = new Random(1);
-
-function resetRnd(seed){
-  g_myRnd = new Random(seed); // パターンIDを乱数シードにする
-}
-
 // ********** 数値制御関数 **********
 
 // 新規人物のIDを割り振る関数
@@ -76,7 +158,7 @@ function getNewID() {
 
 // 性別決定関数
 function defineMale() {
-  let retVal = (g_Params.maleRatio > g_myRnd.get() * 100) ? true : false;
+  let retVal = (g_Params.maleRatio > g_myRnd.random() * 100.0) ? true : false;
   return retVal;
 }
 
@@ -85,7 +167,7 @@ function defineNumChild() {
   // 小数点も考慮。2.5なら2人か3人。
   let numInt = Math.floor(g_Params.numChild);
   let numDecimal = g_Params.numChild - numInt;
-  numInt = (numDecimal > g_myRnd.get()) ? numInt + 1 : numInt;
+  numInt = (numDecimal > g_myRnd.random()) ? numInt + 1 : numInt;
   return numInt;
 }
 
