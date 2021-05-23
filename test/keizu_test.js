213151452cbda8c7285926ebfabef995553513fc
fujas <githubfujas@gmail.com>
2021-05-23 21:34:04 +0900

    いろんなテストパターンを追加。

--- test/keizu_test.js
+++ test/keizu_test.js
@@ -27,15 +27,55 @@ function Params() {
 function GetParams(ind){
   let array = [
     {
-      m: "基本テスト 子供3人",
+      m: "再現テスト 子供3人",
       a: 41.8,
       p:{ numChild: 3, maleRatio: 51.3, liveRatio: 92.1, generation: 30, numFamilyStart: 1, numFamilyMax: 5, maxAnc: 5 }
     },
     {
-      m: "基本テスト 子供3.5人",
+      m: "再現テスト 子供3.5人",
       a: 63.7,
       p:{ numChild: 3.5, maleRatio: 51.3, liveRatio: 92.1, generation: 30, numFamilyStart: 1, numFamilyMax: 5, maxAnc: 5 }
-    }
+    },
+    {
+      m: "確率テスト子供2人50％3世代",
+      a: 61.0,
+      p:{ numChild: 2, maleRatio: 50, liveRatio: 100, generation: 3, numFamilyStart: 1, numFamilyMax: 5, maxAnc: 5 }
+    },
+    {
+      m: "確率テスト子供3人2世代",
+      a: 87.5,
+      p:{ numChild: 3, maleRatio: 50, liveRatio: 100, generation: 2, numFamilyStart: 1, numFamilyMax: 5, maxAnc: 5 }
+    },
+    {
+      m: "子を残す割合のテスト",
+      a: 25.0,
+      p:{ numChild: 1, maleRatio: 100, liveRatio: 50, generation: 3, numFamilyStart: 1, numFamilyMax: 5, maxAnc: 5 }
+    },
+    {
+      m: "http://ore-dmng.jp/ore/lra/prob_dankei.html のテスト",
+      a: 39.9,
+      p:{ numChild: 2, maleRatio: 50, liveRatio: 100, generation: 6, numFamilyStart: 1, numFamilyMax: 10, maxAnc: 6 }
+    },
+    {
+      m: "http://waqoo.blog16.fc2.com/blog-entry-83.html の複数宮家テスト1",
+      a: 76.6,
+      p:{ numChild: 2, maleRatio: 50, liveRatio: 100, generation: 4, numFamilyStart: 2, numFamilyMax: 10, maxAnc: 5 }
+    },
+    {
+      m: "http://waqoo.blog16.fc2.com/blog-entry-83.html の複数宮家テスト2",
+      a: 97.3,
+      p:{ numChild: 2, maleRatio: 50, liveRatio: 100, generation: 4, numFamilyStart: 5, numFamilyMax: 10, maxAnc: 5 }
+    },
+    {
+      m: "https://www.a-takamori.com/post/201013 の直系限定テスト1（遠縁1）",
+      a: 10.0,
+      p:{ numChild: 2, maleRatio: 50, liveRatio: 100, generation: 9, numFamilyStart: 1, numFamilyMax: 5, maxAnc: 1 }
+    },
+    {
+      m: "https://www.a-takamori.com/post/201013 の直系限定テスト2（宮家１）",
+      a: 10.0,
+      p:{ numChild: 2, maleRatio: 50, liveRatio: 100, generation: 9, numFamilyStart: 1, numFamilyMax: 1, maxAnc: 5 }
+    },
   ];
 
   if (ind >= array.length){
@@ -85,6 +125,7 @@ function TestMain() {
 
   // 最終結果表示
   console.log("テスト数", i, "成功数", success);
+  console.log((i == success) ? "***** テスト成功 *****" : "### テスト失敗 ###");
 
   // テストフラグを戻す
   g_IsTest = false;
